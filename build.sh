42a26736140b75321b798036cc23b51b882266ef
John <adeniyijohn2002@gmail.com>
2025-04-30 18:15:36 +0100

    Fix #4

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-#build.sh 
-
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+
+# Explicitly set SECRET_KEY for the collectstatic command
+export SECRET_KEY="$SECRET_KEY"
 python manage.py collectstatic --noinput
+
 python manage.py migrate
